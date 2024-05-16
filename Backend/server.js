1c7d78fd76ef3f25e7bcc23d774d98b1706bc11f
AnkurDamor1110 <ankurdamor1110@gmail.com>
2024-05-16 19:53:38 +0530

    creaate React app and for backend server

---
+++ Backend/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const dbconfig = require("./config/dbconfig");
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});

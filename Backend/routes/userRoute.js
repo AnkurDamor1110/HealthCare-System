34cd2ce312a959d1bd1c7ea4281d683a21f95454
AnkurDamor1110 <ankurdamor1110@gmail.com>
2024-05-18 00:47:34 +0530

    Doctor Display on Home page

--- Backend/routes/userRoute.js
+++ Backend/routes/userRoute.js
@@ -116,5 +116,15 @@ router.post('/delete-all-notification', authMiddleware, async (req, res) => {
     }
 });
 
+router.get('/get-all-approved-doctors',authMiddleware, async (req, res) => {
+    try {
+       const doctors = await Doctor.find({ status: "approved"});
+       res.status(200).send({ message: "Doctors fetched successfully", success: true, data: doctors });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({ message: "Error creating ", success: false, error });
+    }
+});
+
 
 module.exports = router;
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,19 +1,32 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import Layout from '../components/Layout';
+import { Col, Row } from 'antd';
+import Doctor from '../components/Doctor';
+import { useDispatch } from 'react-redux';
+import { hideLoading, showLoading } from '../redux/alertsSlice';
+
 function Home() {
 
+    const [doctors,setDoctor] = useState([]);
+    const dispatch = useDispatch();
     const getData = async () => {
         try {
-            const response = await axios.post('/api/user/get-user-info-by-id', {},
+            dispatch(showLoading());
+            const response = await axios.get('/api/user/get-all-approved-doctors',
                 {
                     headers: {
                         Authorization: `Bearer ` + localStorage.getItem('token'),
                     },
                 }
             );
+            dispatch(hideLoading());
+            if(response.data.success){
+                setDoctor(response.data.data);
+            }
         } catch (error) {
-            console.log(error);
+            dispatch(hideLoading());
+           
         }
     }
 
@@ -22,7 +35,13 @@ function Home() {
     },[]);
 
     return <Layout>
-        <h1>HomePage</h1>
+        <Row>
+            {doctors.map((doctor) =>(
+                <Col span={8} xs={24} sm={24} lg={8}>
+                    <Doctor doctor={doctor}/>
+                </Col>
+            ))}
+        </Row>
     </Layout>
 }
 

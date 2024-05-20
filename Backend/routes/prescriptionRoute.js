1418fbd02c28e5d8833a3a50bb20a7c64304e45c
AnkurDamor1110 <ankurdamor1110@gmail.com>
2024-05-21 01:23:56 +0530

    updated prescription Routes

--- Backend/routes/prescriptionRoute.js
+++ Backend/routes/prescriptionRoute.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const router = express.Router();
 const { getPrescriptions, savePrescription } = require("../controllers/prescriptionController");
+const Prescription = require('../models/prescriptionModel');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 router.post("/prescriptions", savePrescription);
 router.get("/prescriptions", getPrescriptions);
+
+
+
+router.get('/get-prescriptions-by-user-id', authMiddleware, async (req, res) => {
+  try {
+    const userId = req.user._id; // Assuming you get the user ID from the auth middleware
+    const prescriptions = await Prescription.find({ patientId: userId });
+    res.status(200).send({ success: true, data: prescriptions });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ success: false, message: 'Error fetching prescriptions', error });
+  }
+});
+
 
 module.exports = router;

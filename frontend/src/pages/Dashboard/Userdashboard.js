85d70fb7d32c7a549ff9b93184ec0312c2721196
Dhruvin2910 <dhruvinpatel321@gmail.com>
2024-06-27 07:52:08 +0530

    Added pie chart in UserDashboard

--- frontend/src/pages/Dashboard/Userdashboard.js
+++ frontend/src/pages/Dashboard/Userdashboard.js
@@ -3,7 +3,24 @@ import { useDispatch } from "react-redux";
 import { hideLoading, showLoading } from "../../redux/alertsSlice";
 import axios from "axios";
 import Layout from "../../components/Layout";
-
+import { Pie } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  ArcElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  ArcElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 function Userdashboard() {
   const [userDoctor, setuserDoctor] = useState([]);
@@ -78,7 +95,7 @@ function Userdashboard() {
     try {
       dispatch(showLoading());
       const response = await axios.get(
-        "/api/user/get-pading-appointments-by-user-id",
+        "/api/user/get-panding-appointments-by-user-id",
         {
           headers: {
             Authorization: `Bearer ` + localStorage.getItem("token"),
@@ -151,7 +168,35 @@ function Userdashboard() {
     }
   };
 
-  
+  const pieData = {
+    labels: ["Success", "Approved", "Rejected", "Pending"],
+    datasets: [
+      {
+        data: [
+          sucessAppointments.length,
+          approvedAppointments.length,
+          rejectedAppointments.length,
+          pendingAppointments.length,
+        ],
+        backgroundColor: ["#36A2EB", "#FFCE56", "#4BC0C0", "#FF6384"],
+        hoverBackgroundColor: ["#36A2EB", "#FFCE56", "#4BC0C0", "#FF6384"],
+      },
+    ],
+  };
+  const pieOptions = {
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+      legend: {
+        position: "top",
+      },
+      title: {
+        display: true,
+        text: "Appointments Overview",
+      },
+    },
+  };
+
   useEffect(() => {
     getPrescriptions();
     getAppointments();
@@ -168,20 +213,60 @@ function Userdashboard() {
         User Dashboard
       </title>
       <div className="flex">
-        <div className="bg-white mx-2 ml-5 my-3 p-3 rounded-lg w-1/2 flex justify-between items-center shadow-md"><i className="fa-solid fa-user-doctor fa-xl"></i><h1 className="flex justify-center font-semibold">User's Doctor: </h1><p className="font-bold text-2xl">{userDoctor.length}</p></div>
-        <div className="bg-white mx-1 mr-5 my-3 p-3 rounded-lg w-1/2 flex justify-between items-center shadow-md"><i class="fa-solid fa-calendar-days fa-xl"></i><h1 className="flex justify-center font-semibold">Appointments: </h1><p className="font-bold text-2xl">{appointments.length}</p></div>
+        <div className="bg-white mx-2 ml-5 my-3 p-3 rounded-lg w-1/2 flex justify-between items-center shadow-md">
+          <i className="fa-solid fa-user-doctor fa-xl"></i>
+          <h1 className="flex justify-center font-semibold">User's Doctor: </h1>
+          <p className="font-bold text-2xl">{userDoctor.length}</p>
+        </div>
+        <div className="bg-white mx-1 mr-5 my-3 p-3 rounded-lg w-1/2 flex justify-between items-center shadow-md">
+          <i class="fa-solid fa-calendar-days fa-xl"></i>
+          <h1 className="flex justify-center font-semibold">Appointments: </h1>
+          <p className="font-bold text-2xl">{appointments.length}</p>
+        </div>
       </div>
       <div className=" flex">
-      <div className="bg-white m-2 p-3 rounded-lg h-20 w-1/3 flex justify-between items-center shadow-md"><i class="fa-solid fa-square-check fa-xl"></i><h1 className="flex justify-center font-semibold">Success Appointments : </h1><p className="font-bold text-2xl">{sucessAppointments.length}</p></div>
-      <div className="bg-white m-2 p-3 rounded-lg h-20 w-1/3 flex justify-between items-center shadow-md"><i class="fa-solid fa-calendar-check fa-xl"></i><h1 className="flex justify-center font-semibold">Approved Appointments: </h1><p className="font-bold text-2xl">{approvedAppointments.length}</p></div>
-      <div className="bg-white m-2 p-3 rounded-lg h-20 w-1/3 flex justify-between items-center shadow-md"><i class="fa-regular fa-calendar-xmark fa-xl"></i><h1 className="flex justify-center font-semibold">Rejected Appointments: </h1><p className="font-bold text-2xl">{rejectedAppointments.length}</p></div>
+        <div className="bg-white m-2 p-3 rounded-lg h-20 w-1/3 flex justify-between items-center shadow-md">
+          <i class="fa-solid fa-square-check fa-xl"></i>
+          <h1 className="flex justify-center font-semibold">
+            Success Appointments :{" "}
+          </h1>
+          <p className="font-bold text-2xl">{sucessAppointments.length}</p>
+        </div>
+        <div className="bg-white m-2 p-3 rounded-lg h-20 w-1/3 flex justify-between items-center shadow-md">
+          <i class="fa-solid fa-calendar-check fa-xl"></i>
+          <h1 className="flex justify-center font-semibold">
+            Approved Appointments:{" "}
+          </h1>
+          <p className="font-bold text-2xl">{approvedAppointments.length}</p>
+        </div>
+        <div className="bg-white m-2 p-3 rounded-lg h-20 w-1/3 flex justify-between items-center shadow-md">
+          <i class="fa-regular fa-calendar-xmark fa-xl"></i>
+          <h1 className="flex justify-center font-semibold">
+            Rejected Appointments:{" "}
+          </h1>
+          <p className="font-bold text-2xl">{rejectedAppointments.length}</p>
+        </div>
       </div>
       <div className=" flex justify-center">
-      <div className="bg-white m-2 p-3 rounded-lg h-20 w-1/3 flex justify-between items-center shadow-md"><i class="fa-solid fa-calendar-day fa-xl"></i><h1 className="flex justify-center font-semibold">Pending Appointments: </h1><p className="font-bold text-2xl">{pendingAppointments.length}</p></div>
-      <div className="bg-white m-2 p-3 rounded-lg h-20 w-1/3 flex justify-between items-center shadow-md"><i class="fa-solid fa-clipboard fa-xl"></i><h1 className="flex justify-center font-semibold">Prescriptions Appointments: </h1><p className="font-bold text-2xl">{prescriptions.length}</p></div>
-      </div> 
-      <div>
-        
+        <div className="bg-white m-2 p-3 rounded-lg h-20 w-1/3 flex justify-between items-center shadow-md">
+          <i class="fa-solid fa-calendar-day fa-xl"></i>
+          <h1 className="flex justify-center font-semibold">
+            Pending Appointments:{" "}
+          </h1>
+          <p className="font-bold text-2xl">{pendingAppointments.length}</p>
+        </div>
+        <div className="bg-white m-2 p-3 rounded-lg h-20 w-1/3 flex justify-between items-center shadow-md">
+          <i class="fa-solid fa-clipboard fa-xl"></i>
+          <h1 className="flex justify-center font-semibold">
+            Prescriptions Appointments:{" "}
+          </h1>
+          <p className="font-bold text-2xl">{prescriptions.length}</p>
+        </div>
+      </div>
+      <div className="chart flex justify-center">
+        <div className="pie-chart">
+          <Pie data={pieData} />
+        </div>
       </div>
     </Layout>
   );

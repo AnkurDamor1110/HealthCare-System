921ce986f3ae36d93090da3c370d0932931f4f48
Sahil machhar <machharsahil9211@gmail.com>
2024-07-03 16:02:03 +0530

    validation in email for contact in backend

--- Backend/models/contactModel.js
+++ Backend/models/contactModel.js
@@ -4,6 +4,11 @@ const contactSchema = new mongoose.Schema({
 
     email: {
         type : String,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("Email is not valid");
+            }
+        },
         require : true
     },
     subject: {
